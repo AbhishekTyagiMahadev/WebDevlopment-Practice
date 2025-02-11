81a66fd464e936aa135ee26ce9ead4377922aa57
AbhishekTyagiMahadev <abhishektyagi250342@gmail.com>
2025-02-11 13:49:35 +0530

    Learn Variables and data types

---
+++ JavaScript/2-Variable&DataType/script.js
@@ -0,0 +1,43 @@
+// Variable declaration
+var name = "Abhishek Tyagi"; // String
+var age = 30; // Numbern
+var rollno = 1009; // Number
+
+// Print the variables
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Roll Number:", rollno);
+
+
+// Data types in JavaScript
+// 1. Primitive Data Types
+// Null
+// Number
+// String
+// Symbol
+// Undefined
+// Boolean
+// BigInt
+
+
+// 2. Reference Data Types
+// Arrays
+// Object Literals
+// Functions
+// Dates
+
+{
+    let age = 40;
+    console.log(age)
+}
+console.log(age)
+
+// Objects
+let person = {
+    name: "Abhishek Tyagi Mahadev",
+    age: 21,
+    rollno: 1009
+}
+console.log(person)
+person.mobile = 1234567890
+console.log(person)
