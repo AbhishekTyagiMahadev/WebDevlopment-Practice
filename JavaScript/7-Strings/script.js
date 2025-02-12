1277ccfd1b44acfee4dae7a6c57044cd7653db66
AbhishekTyagiMahadev <abhishektyagi250342@gmail.com>
2025-02-12 15:14:49 +0530

    Learn Strings

---
+++ JavaScript/7-Strings/script.js
@@ -0,0 +1,36 @@
+console.log("This is strings tutorial")
+let a = "Abhishek Tyagi";
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+console.log(a[5]);
+console.log(a[6]);
+console.log(a[7]);
+console.log(a[8]);
+console.log(a[9]);
+console.log(a[10]);
+console.log(a[11]);
+console.log(a[12]);
+console.log(a[13]);
+
+console.log(a.length)
+
+let real_name = "Abhishek"
+let friend = "Vivek"
+console.log("His name is " + real_name + " and his friends name is " + friend)
+console.log(`His name is ${real_name} and his friends name is ${friend}`)
+
+let b = "Aditya "
+
+// console.log(b.toUpperCase())
+// console.log(b.toLowerCase()) 
+// console.log(b.length) 
+// console.log(b.slice(1, 5)) 
+// console.log(b.slice(1)) 
+// console.log(b.replace("Sh", "77"))
+
+console.log(b.concat("Puri ", a, " Vivek", " Himanshi", " Tanu"))
+
+console.log(b)
