25f576a5a82b6773b5fe0a4919171322d400e0b0
AbhishekTyagiMahadev <abhishektyagi250342@gmail.com>
2025-02-12 21:35:41 +0530

    Creating Random Name Generator

---
+++ JavaScript/script.js
@@ -0,0 +1,20 @@
+let ran1 = Math.floor(Math.random() * (3)) + 1;
+let ran2 = Math.floor(Math.random() * (3)) + 1;
+let ran3 = Math.floor(Math.random() * (3)) + 1;
+
+let Adjectives = {
+    "1": "Crazy",
+    "2": "Amazing",
+    "3": "Fire"
+};
+let Name = {
+    "1": "Engine",
+    "2": "Foods",
+    "3": "Germents"
+};
+let Another = {
+   "1": "Bros",
+    "2": "Limited",
+    "3": "Hub"
+};
+console.log(Adjectives[ran1], Name[ran2], Another[ran3])
