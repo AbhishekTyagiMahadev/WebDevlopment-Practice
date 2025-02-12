a6a2c3a2b350e9c52b07add802b02ec5f1cf8b60
AbhishekTyagiMahadev <abhishektyagi250342@gmail.com>
2025-02-12 07:22:12 +0530

    Learn Conditionals if-else and Switch statements

---
+++ JavaScript/3-Conditionals/script.js
@@ -0,0 +1,48 @@
+console.log("Hello World!");
+
+var age = 30;
+
+// If-else statement
+if (age < 18) {
+    console.log("You are a minor.");
+} else if (age >= 18 && age < 65) {
+    console.log("You are an adult.");
+} else {
+    console.log("You are a senior citizen.");
+}
+
+// Turnary operator
+var isAdult = age >= 18 ? "Yes" : "No";
+console.log("Are you an adult?", isAdult);
+
+// Switch statement
+var day = 4;
+var dayName;
+
+switch (day) {
+    case 1:
+        dayName = "Monday";
+        break;
+    case 2:
+        dayName = "Tuesday";
+        break;
+    case 3:
+        dayName = "Wednesday"
+        break;
+    case 4:
+        dayName = "Thursday";
+        break;
+    case 5:
+        dayName = "Friday";
+        break;
+    case 6:
+        dayName = "Saturday";
+        break;
+    case 7:
+        dayName = "Sunday";
+        break;
+    default:
+        dayName = "Invalid day";
+}
+
+console.log("Today is:", dayName);
