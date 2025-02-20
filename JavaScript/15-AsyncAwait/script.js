62ea93967f0f7d268a1f04349cc9fec2aae32ecc
AbhishekTyagiMahadev <abhishektyagi250342@gmail.com>
2025-02-20 16:11:51 +0530

    Learn About Async Await

--- JavaScript/15-AsyncAwait/script.js
+++ JavaScript/15-AsyncAwait/script.js
@@ -1,21 +1,3 @@
-// function getdata(){
-//     return new Promise((resolve,reject)=>{
-//         setTimeout(()=>{
-//             resolve("Data Fetched");
-//         },3000)
-//     })
-// }
-console.log("Fetching Data");
-
-async function main() {
-
-    // console.log("Fetching Data");
-    let data = await fetchapi();
-    console.log(data);
-
-    console.log("Hello World");
-}
-main();
 
 // Featch API
 async function fetchapi() {
@@ -26,3 +8,43 @@ async function fetchapi() {
     console.log(data);
     return "APT Data Fetched";
 }
+console.log("Fetching Data");
+
+async function main() {
+    console.log(await fetchapi());
+    console.log("Main Function Executed");
+}
+main();
+
+
+async function getdata(data) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            console.log("Data = " + data);
+            resolve();
+        }, 3000);
+    });
+}
+
+// IIFE(Immeditately Invoked Function Expressiion) Function
+// (async function () {
+//     await getdata(100);
+//     await getdata(200);
+//     await getdata(300);
+//     await getdata(400);
+//     await getdata(500);
+//     console.log("IIFE Function Executed");
+// })()
+
+
+
+
+async function main2() {
+    await getdata(10);
+    await getdata(20);
+    await getdata(30);
+    await getdata(40);
+    await getdata(50);
+    console.log("Main2 Function Executed");
+}
+main2();
